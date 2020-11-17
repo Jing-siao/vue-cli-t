9ce984a9d1c5ef0e7dfd87a8da58b31a619331e3
LEOSYS\jing <jing@leosys.com>
2020-11-17 17:32:20 +0800

    練習 router練習 router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Jing from '../views/Jing.vue'
+
 
 Vue.use(VueRouter)
 
@@ -16,8 +18,14 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
+    //此方法可以省下一開始讀取時間 但就是換頁會多一次讀取
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  {
+    path: '/Jing',
+    name: 'Jing',
+    component: Jing
+  },
 ]
 
 const router = new VueRouter({
---
+++ src/views/Jing.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Jing</h1>
+</template>
