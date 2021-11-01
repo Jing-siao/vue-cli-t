62ab75b6be148539b0ac67f2902d1521398fa0e9
jing <vicky820715@gmail.com>
2021-11-01 14:57:07 +0800

    新增pwa

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ import App from './App.vue'
 // import "./registerServiceWorker";
 import router from './router'
 import store from './store'
+import './registerServiceWorker'
 
 Vue.config.productionTip = false
 Vue.prototype.axios = axios
--- vue.config.js
+++ vue.config.js
@@ -20,6 +20,22 @@ module.exports = {
       }
     }
   },
+  pwa: {
+    name: 'CSDN',
+    themeColor: '#4DBA87',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+
+    // 配置 workbox 插件
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      // InjectManifest 模式下 swSrc 是必填的。
+      swSrc: './public/service-worker.js', // 本地sw.js相对路径
+      // importWorkboxFrom: 'disabled', // 决定是否引入workbox
+      exclude: [/\.html$/, /\.css.map$/, /sw.js$/] // 缓存列表排除的文件，可以是文件名或正则
+    }
+  },
   // publicPath: process.env.NODE_ENV === 'production'
   //   ? '/production-sub-path/'
   //   : '/'
@@ -28,7 +44,7 @@ module.exports = {
   publicPath: './',
   // 輸出檔案目錄
   outputDir: 'dist',
-  filenameHashing: false
+  filenameHashing: false,
   // assetsDir: '/',
 
 };
